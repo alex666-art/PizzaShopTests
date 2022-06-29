9c80ae0edee2b40bc5efc5e43964cce03839f1a7
alex666-art <alexandr-makedonsky@yandex.ru>
2022-06-29 13:06:04 +0300

    addPizzaPageTests

--- src/test/java/all/pizzaShop/PizzaTests.java
+++ src/test/java/all/pizzaShop/PizzaTests.java
@@ -1,12 +1,15 @@
 package all.pizzaShop;
+
 import all.base.TestBase;
 import all.pizzaShop.pages.MainPage;
+import all.pizzaShop.pages.PizzaPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -16,7 +19,7 @@ public class PizzaTests extends TestBase {
     private String errorMessage = "Элемент не отображается!";
 
     @Test
-    public void scrollCarouselForward__andDisplayCard__Test() {
+    public void mainPage__scrollCarouselForward__andDisplayCard__test() {
         //arrange
         var page = new MainPage(driver, wait);
         page.open();
@@ -28,7 +31,7 @@ public class PizzaTests extends TestBase {
     }
 
     @Test
-    public void scrollCarouselBack__andDisplayCard__Test() {
+    public void mainPage__scrollCarouselBack__andDisplayCard__test() {
         //arrange
         var page = new MainPage(driver, wait);
         page.open();
@@ -40,7 +43,7 @@ public class PizzaTests extends TestBase {
     }
 
     @Test
-    public void moveToCard__click__andDisplayTitle__Test() {
+    public void mainPage__moveToCard__click__andDisplayTitle__test() {
         //arrange
         var page = new MainPage(driver, wait);
         page.open();
@@ -53,7 +56,7 @@ public class PizzaTests extends TestBase {
     }
 
     @Test
-    public void moveToFooter__andDisplayUpButton__Test() {
+    public void mainPage__moveToFooter__andDisplayUpButton__test() {
         //arrange
         var page = new MainPage(driver, wait);
         page.open();
@@ -64,6 +67,7 @@ public class PizzaTests extends TestBase {
         var expectedResult = page.upButton;
         Assertions.assertTrue(expectedResult.isDisplayed(), errorMessage);
     }
+
     private static Stream<Arguments> indexAndUrl__links() {
         return Stream.of(
                 arguments(3, "https://www.facebook.com/skillboxru"),
@@ -74,7 +78,7 @@ public class PizzaTests extends TestBase {
 
     @ParameterizedTest
     @MethodSource("indexAndUrl__links")
-    public void moveToFooter__andClickSocialLinks__Test(int index, String url) {
+    public void mainPage__moveToFooter__andClickSocialLinks__test(int index, String url) {
         //arrange
         var page = new MainPage(driver, wait);
         page.open();
@@ -89,5 +93,72 @@ public class PizzaTests extends TestBase {
                 () -> Assertions.assertEquals(2, getAllWindows().size(), "Вторая вкладка не открылась!"),
                 () -> Assertions.assertEquals(expectedUrl, driver.getCurrentUrl(), "Открылась не верная страница!")
         );
+    }
+
+    private static Stream<Arguments> selectValues() {
+        return Stream.of(
+                arguments("menu_order", "Обычная сортировка"),
+                arguments("popularity", "По популярности"),
+                arguments("date", "Последние"),
+                arguments("price", "По возрастанию цены"),
+                arguments("price-desc", "По убыванию цены")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("selectValues")
+    public void pizzaPage__getSelectValue__test(String value, String expectedResult) {
+        //arrange
+        var page = new PizzaPage(driver, wait);
+        page.open();
+        var initialValue = page.getSorted();
+        //action
+        page.setSorted(value);
+        //assert
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expectedResult, page.getSorted(), "Выбрано неверное значение!"),
+                () -> Assertions.assertTrue(page.productCards.isDisplayed(), "Карточки не отображаются!")
+        );
+    }
+
+    @Test
+    public void pizzaPage__getSelectValues__test() {
+        //arrange
+        var page = new PizzaPage(driver, wait);
+        page.open();
+        //action
+        var expectedSorts = Arrays.asList("Обычная сортировка", "По популярности", "Последние", "По возрастанию цены", "По убыванию цены");
+        //assert
+        Assertions.assertEquals(expectedSorts, page.getSortedList(), "В списке сортировок присутствуют не все значения!");
+    }
+
+    @Test
+    public void pizzaPage__getCardsSize__test() {
+        //arrange
+        var page = new PizzaPage(driver, wait);
+        page.open();
+        //action
+        page.moveSlider();
+        page.submitButton.click();
+        page.waitTitle();
+        //assert
+        var expectedSize = "1";
+        var expectedTitle = "Пицца «Рай»";
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expectedSize, page.getCardSize(), "Колличество карточек не совпадает с ожидаемым результатом!"),
+                () -> Assertions.assertEquals(expectedTitle, page.getTitleText(), "Фильтрация по цене не работает!")
+        );
+    }
+
+    @Test
+    public void pizzaPage__buyPizzaAndDisplayCardOnBasket__test() {
+        //arrange
+        var page = new PizzaPage(driver, wait);
+        page.open();
+        //action
+        page.addPizzaToTheBasket();
+        //assert
+        var expectedResult = "[ 435,00₽ ]";
+        Assertions.assertEquals(expectedResult, page.getPriceText(), "Товар не добавился в корзину!");
     }
 }
--- src/test/java/all/pizzaShop/pages/PizzaPage.java
+++ src/test/java/all/pizzaShop/pages/PizzaPage.java
@@ -1,5 +1,74 @@
 package all.pizzaShop.pages;
+import all.base.pages.Page;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class PizzaPage {
+public class PizzaPage extends Page {
 
+    private String url = "http://pizzeria.skillbox.cc/product-category/menu/pizza/";
+
+    @FindBy(name = "orderby")
+    public WebElement sortedSelect;
+    @FindBy(css = ".product")
+    public WebElement productCards;
+    @FindBy(css = ".product")
+    public List<WebElement> productCardList;
+    @FindBy(css = ".button:nth-child(3)")
+    public WebElement submitButton;
+    @FindBy(css = ".ui-slider-handle:nth-of-type(1)")
+    public WebElement leftSliderButton;
+    @FindBy(css = ".collection_title")
+    public WebElement cardTitle;
+    @FindBy(css = ".product:nth-child(1) .button")
+    public WebElement buyButton;
+    @FindBy(css = ".added_to_cart")
+    public WebElement goToBasketButton;
+    @FindBy(linkText = "[ 435,00₽ ]")
+    public WebElement price;
+
+    public PizzaPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {driver.navigate().to(url);}
+
+    public void setSorted(String value) {
+        var sorted = new Select(sortedSelect);
+        sorted.selectByValue(value);
+    }
+
+    public String getSorted() {
+        var sorted = new Select(sortedSelect);
+        return sorted.getFirstSelectedOption().getText();
+    }
+
+    public List<String> getSortedList() {
+        var author = new Select(sortedSelect);
+        return author.getOptions().stream().map(element -> element.getText()).collect(Collectors.toList());
+    }
+
+    public String getCardSize() {return String.valueOf(productCardList.size());}
+
+    public void moveSlider() {
+        for (int i = 0; i < 8; i++) {
+            leftSliderButton.sendKeys(Keys.ARROW_RIGHT);
+        }
+    }
+
+    public void waitTitle() {wait.until(driver -> cardTitle.isDisplayed());}
+
+    public String getTitleText() {return cardTitle.getText();}
+
+    public void addPizzaToTheBasket() {buyButton.click();}
+
+    public String getPriceText() {return price.getText();}
 }
